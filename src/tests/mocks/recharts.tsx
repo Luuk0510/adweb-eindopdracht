e940a6239195065f028a0acce32dae5a88ca38ca
N-Wilson11 <91117438+N-Wilson11@users.noreply.github.com>
2026-06-04 13:20:27 +0200

    remove duplicate code

--- src/tests/mocks/recharts.tsx
+++ src/tests/mocks/recharts.tsx
@@ -20,17 +20,10 @@ export function Line({ name }: { name: string }) {
   return <div>{name}</div>;
 }
 
-export function Bar({ name }: { name: string }) {
-  return <div>{name}</div>;
-}
-
 export function LineChart({ children }: { children: ReactNode }) {
   return <div data-testid="line-chart">{children}</div>;
 }
 
-export function BarChart({ children }: { children: ReactNode }) {
-  return <div data-testid="bar-chart">{children}</div>;
-}
 
 export function ResponsiveContainer({ children }: { children: ReactNode }) {
   return <div data-testid="responsive-container">{children}</div>;
